a6d333ffc8b433b6c7b9b3ce795424b543e0a575
developer119 <developer119korea@gmail.com>
2017-07-20 17:48:18 +0900

    Add RoundInfoData

---
+++ PacketRoundInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace NetworkLibrary
+{
+    public class PacketRoundInfo : Packet
+    {
+        private PInteger _maxTurn = new PInteger();
+        private PFloat _leftTime = new PFloat();
+
+        public PacketRoundInfo(int PacketType) : base(PacketType, 2)
+        {
+            _field[0] = _maxTurn;
+            _field[1] = _leftTime;
+        }
+
+        public void InitPacketRoundInfo()
+        {
+            Random random = new Random();
+
+            _maxTurn.n = random.Next();
+            _leftTime.f = random.Next();
+
+            _totalSize.n = (byte)this.GetSize();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(base.ToString());
+
+            sb.Append(string.Format("MaxTurn : {0}, LeftTime : {1}", _maxTurn, _leftTime));
+
+            return sb.ToString();
+        }
+    }
+}
